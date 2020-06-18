11a50a6d91fe4c421267c18359d02131a5f37df0
Raphael Lautenbacher <raphaellautenbacher@raphaellautenbacher.local>
2020-06-18 08:57:28 +0200

    line through for checked tasks

--- main.js
+++ main.js
@@ -3,10 +3,16 @@ const createProjectBtn = document.querySelector(".create-project");
 const projectList = document.querySelector(".project-list");
 const addTaskBtn = document.querySelector(".add-task");
 const checklist = document.querySelector(".checklist");
+const addCollabBtn = document.querySelector(".add-collab");
+const collabList = document.querySelector(".collab-container");
+let counterTaskbar = document.querySelector(".counter");
+const inputCheckbox = document.querySelector(".input-task");
 
 //Event Listeners
 createProjectBtn.addEventListener("click", addProject);
 addTaskBtn.addEventListener("click", addTask);
+addCollabBtn.addEventListener("click", addCollab);
+inputCheckbox.addEventListener("click", taskDone);
 
 //Functions
 function addProject(event) {
@@ -28,4 +34,22 @@ function addTask() {
   newTaskDiv.appendChild(newLabel);
 
   checklist.appendChild(newTaskDiv);
+}
+
+function addCollab() {
+  console.log("clicked");
+  const newCollabDiv = document.createElement("div");
+  newCollabDiv.classList.add("collab-container");
+
+  const newCollabImgDiv = document.createElement("div");
+  newCollabImgDiv.classList.add("collab-circleImg");
+  newCollabImgDiv.style.backgroundImage =
+    "url('https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80')";
+  newCollabDiv.appendChild(newCollabImgDiv);
+
+  collabList.appendChild(newCollabImgDiv);
+}
+
+function taskDone() {
+  console.log(counterTaskbar);
 }
